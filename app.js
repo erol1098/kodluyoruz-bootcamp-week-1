0a56ef6314e2f59a95db773f56ee5e9008b6f94a
erol1098 <erol1098@gmail.com>
2022-07-31 17:04:25 +0300

    Week-1/

--- app.js
+++ app.js
@@ -2,23 +2,38 @@
 const cardBox = document.querySelector(".card-box");
 const searchBar = document.querySelector(".search-bar");
 
-const renderCards = async () => {
+const getData = async () => {
   try {
     const { data } = await axios.get(
       "https://jsonplaceholder.typicode.com/photos?_limit=50"
     );
-
-    data.forEach((photo) => {
-      const { albumId, id, title, thumbnailUrl } = photo;
-      const card = document.createElement("div");
-      card.classList.add("card");
-      card.innerHTML = `<figure>
-        <img src=${thumbnailUrl} alt="Photo ${albumId}/${id}" /> 
-        <figcaption>${title}</figcaption></figure>`;
-      cardBox.append(card);
-    });
+    return data;
   } catch (error) {
     console.log("error", error);
   }
 };
-renderCards();
+
+let photoList;
+getData().then((res) => (photoList = res));
+
+const renderCards = (list) => {
+  cardBox.innerHTML = "";
+  list.forEach((photo) => {
+    const { albumId, id, title, thumbnailUrl } = photo;
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = `<figure>
+      <img src=${thumbnailUrl} alt="Photo ${albumId}/${id}" />
+      <figcaption>${title}</figcaption></figure>`;
+    cardBox.append(card);
+  });
+};
+
+window.addEventListener("load", () => renderCards(photoList));
+
+searchBar.addEventListener("input", (e) => {
+  const filteredList = photoList.filter((photo) =>
+    photo.title.includes(e.target.value)
+  );
+  renderCards(filteredList);
+});
