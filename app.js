b78fada97bb6d9acd10e021edbe0e0eda44908ff
erol1098 <erol1098@gmail.com>
2022-07-31 17:58:38 +0300

    Week-1/Delete Card Added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const cardBox = document.querySelector(".card-box");
 const searchBar = document.querySelector(".search-bar");
 
+//? Get Data From API
 const getData = async () => {
   try {
     const { data } = await axios.get(
@@ -12,16 +13,17 @@ const getData = async () => {
     console.log("error", error);
   }
 };
-
 let photoList;
 getData().then((res) => (photoList = res));
 
+//? Render Cards Method
 const renderCards = (list = []) => {
   cardBox.innerHTML = "";
   list.forEach((photo) => {
     const { albumId, id, title, thumbnailUrl } = photo;
     const card = document.createElement("div");
     card.classList.add("card");
+    card.setAttribute("id", `${albumId}/${id}`);
     card.innerHTML = `<figure>
       <img src=${thumbnailUrl} alt="Photo ${albumId}/${id}" />
       <figcaption>${title}</figcaption></figure>`;
@@ -29,11 +31,19 @@ const renderCards = (list = []) => {
   });
 };
 
+//? First Render
 window.addEventListener("load", () => renderCards(photoList));
 
+//? Filter by search
 searchBar.addEventListener("input", (e) => {
   const filteredList = photoList.filter((photo) =>
     photo.title.includes(e.target.value)
   );
   renderCards(filteredList);
+});
+
+//? Delete Card
+cardBox.addEventListener("click", (e) => {
+  const deletedCard = e.target.closest(".card");
+  cardBox.removeChild(document.getElementById(deletedCard.id));
 });
